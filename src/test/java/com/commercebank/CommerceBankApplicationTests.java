f0ae145d0f70713c6e54da2981313a38138c5cbd
dbruce <dbruce@ucmo.edu>
2019-02-03 15:03:42 -0600

    Added dirt simple test file

--- src/test/java/com/commercebank/CommerceBankApplicationTests.java
+++ src/test/java/com/commercebank/CommerceBankApplicationTests.java
@@ -1,5 +1,6 @@
 package com.commercebank;
 
+import com.commercebank.api.Branch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommerceBankApplicationTests {
+
 
 	@Test
 	public void contextLoads() {
