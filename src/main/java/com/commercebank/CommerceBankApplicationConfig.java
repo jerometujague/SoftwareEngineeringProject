2c5c0d278181a4e0350201c103f80c9ba3945939
Jon Bockhorst <jmbockhorst@gmail.com>
2019-02-03 14:23:39 -0600

    Added DataSource builder for DAO

--- src/main/java/com/commercebank/CommerceBankApplicationConfig.java
+++ src/main/java/com/commercebank/CommerceBankApplicationConfig.java
@@ -1,8 +1,19 @@
 package com.commercebank;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class CommerceBankApplicationConfig {
-
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
 }
