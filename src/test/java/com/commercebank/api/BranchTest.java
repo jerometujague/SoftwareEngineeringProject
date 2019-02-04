98fce71d4e078f5a97ec24ff24bd2e15e562948c
dbruce <dbruce@ucmo.edu>
2019-02-04 14:56:46 -0600

    added spring to BranchTest

--- src/test/java/com/commercebank/api/BranchTest.java
+++ src/test/java/com/commercebank/api/BranchTest.java
@@ -1,9 +1,13 @@
 package com.commercebank.api;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
                                     // This class only tests the Branch class
 public class BranchTest {
 
