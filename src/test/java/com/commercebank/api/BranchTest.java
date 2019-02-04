b19ab44902759b4bd35e1aba829d053b70709ee3
dbruce <dbruce@ucmo.edu>
2019-02-03 19:36:28 -0600

    formatted comments

--- src/test/java/com/commercebank/api/BranchTest.java
+++ src/test/java/com/commercebank/api/BranchTest.java
@@ -4,18 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-// This class only tests the Branch class
+                                    // This class only tests the Branch class
 public class BranchTest {
 
-    // Create an instance of the Branch object
+                                    // Create an instance of the Branch object
     Branch someBranch = new Branch(3,"123 Main St.");
 
-    @Test           // Create a test method for the getID method
+    @Test                           // Create a test method for the getID method
     public void testGetID() {
 
-        // This test makes the assertion that the ID for someBranch
-        // should be equal to 3. When the test is run, the test will be
-        // passed if someBranch.getID() returns a value of 3.
+                                    /* This test makes the assertion that the ID for someBranch
+                                     * should be equal to 3. When the test is run, the test will be
+                                     * passed if someBranch.getID() returns a value of 3. */
         assertEquals(3, someBranch.getId());
     }
 }
