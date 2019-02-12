0074adfeb04107791a513a4812157459764564f8
dbruce <dbruce@ucmo.edu>
2019-02-12 13:51:04 -0600

    added ServiceMapper class

---
+++ src/main/java/com/commercebank/mapper/ServiceMapper.java
@@ -0,0 +1,20 @@
+package com.commercebank.mapper;
+
+import com.commercebank.model.Service;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ServiceMapper implements RowMapper<Service> {
+    @Override
+    public Service mapRow(ResultSet rs, int rowNum) throws SQLException {
+        // Create a new branch object that is mapped to the SQL Result rs
+        return new Service(
+                rs.getInt("id"),
+                rs.getString("service"));
+
+    }
+}
