eccdf42aa38f9cea4a15aad0f1221399f116043f
Jon Bockhorst <jmbockhorst@gmail.com>
2019-03-01 09:32:28 -0600

    Added ESLint for code checking

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "plugins": [
+        "react"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true,
+        "mocha": true,
+        "jquery": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "rules": {
+        // "react/prop-types": 0
+    }
+}
