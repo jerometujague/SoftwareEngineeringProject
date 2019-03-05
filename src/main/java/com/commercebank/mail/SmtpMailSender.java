9702739bd830650de091b148072cf0d019046875
dbruce <dbruce@ucmo.edu>
2019-03-05 14:35:32 -0600

    email sends with calendar invite

--- src/main/java/com/commercebank/mail/SmtpMailSender.java
+++ src/main/java/com/commercebank/mail/SmtpMailSender.java
@@ -1,9 +1,11 @@
 package com.commercebank.mail;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 
 public class SmtpMailSender implements MailSender{
@@ -15,7 +17,7 @@ public class SmtpMailSender implements MailSender{
     }
 
     @Override
-    public void send(String to, String cc, String subject, String body) throws MessagingException {
+    public void send(String to, String cc, String subject, String body, String attach) throws MessagingException {
 
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper;
@@ -26,8 +28,9 @@ public class SmtpMailSender implements MailSender{
         helper.setText(body, true);     //true indicates html
         helper.setFrom("UCM SE3910 Project <seclassproject@gmail.com>");
         helper.setCc(cc);
+        FileSystemResource file = new FileSystemResource(new File(attach));
+        helper.addAttachment(attach, file);
 
-        // continue using helper for more functionalities like adding attachments, etc.
 
         javaMailSender.send(message);
     }
--- src/main/java/com/commercebank/util/DateUtil.java
+++ src/main/java/com/commercebank/util/DateUtil.java
@@ -1,14 +1,18 @@
 package com.commercebank.util;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+
 public class DateUtil {
     // Get the 12 hour time from a hour
-    public static String hourToHumanString(int hour){
+    public static String hourToHumanString(int hour) {
         String timeString = String.valueOf(hour) + ":00 AM";
 
         // Check for PM time
-        if(hour > 12){
+        if (hour > 12) {
             timeString = String.valueOf(hour - 12) + ":00 PM";
-        } else if(hour == 12){
+        } else if (hour == 12) {
             timeString = "12:00 PM";
         }
 
@@ -17,7 +21,7 @@ public class DateUtil {
 
     // Get a number with the ordinal of that number
     public static String ordinal(int i) {
-        String[] sufixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        String[] sufixes = new String[]{"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"};
         switch (i % 100) {
             case 11:
             case 12:
@@ -29,7 +33,16 @@ public class DateUtil {
     }
 
     // Capitalize a string
-    public static String capitalize(String s){
+    public static String capitalize(String s) {
         return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+    }
+
+    //get current timestamp
+    public static String getTimeStamp() {
+        String currentTime = ZonedDateTime.now(ZoneId.of("UTC")).toString();
+        StringBuilder str = new StringBuilder(currentTime);
+        str.deleteCharAt(4).deleteCharAt(6).deleteCharAt(11).deleteCharAt(13).deleteCharAt(15).deleteCharAt(17);
+        str.delete(18, 22).deleteCharAt(str.length() - 1);
+        return str.toString();
     }
 }
