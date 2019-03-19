df3c6d1196a8b3a617633073e8ba2558b71323c9
Jerome  Tujague <jerometujague@yahoo.com>
2019-03-19 14:17:11 -0500

    Added input validation for user input

--- src/main/resources/assets/src/infoPage.js
+++ src/main/resources/assets/src/infoPage.js
@@ -10,6 +10,7 @@ export default class Info extends React.Component {
             lastName: '',
             phoneNumber: '',
             email: '',
+            errors: {}
         }
     }
 
@@ -23,10 +24,57 @@ export default class Info extends React.Component {
         });
     }
 
+    validateInput(email, firstName, lastName, phoneNumber){
+        let formIsValid = true;
+        let errors = {};
+
+        if(email == ''){
+                     formIsValid = false;
+                     errors["email"] = "Please enter your email address.";
+                 }
+
+        if(firstName == ''){
+            formIsValid = false;
+            errors["firstName"] = "Please enter your first name.";
+        }
+
+        if(lastName == ''){
+            formIsValid = false;
+            errors["lastName"] = "Please enter your last name.";
+        }
+
+        if(phoneNumber == ''){
+            formIsValid = false;
+            errors["phoneNumber"] = "Please enter your phoneNumber.";
+        }
+
+        //checks for valid email address
+        if (email !== "undefined") {
+            //regular expression for email validation
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(email)) {
+              formIsValid = false;
+              errors["email"] = "Please enter valid email address.";
+            }
+        }
+
+        //checks for valid phone number
+        if (phoneNumber !== "undefined") {
+            if (!phoneNumber.match(/^[0-9]{10}$/)) {
+                formIsValid = false;
+                errors["phoneNumber"] = "Please enter valid phone number.";
+            }
+        }
+
+       this.setState({
+            errors: errors
+       });
+       return formIsValid;
+    }
+
     async scheduleAppointment() {
-        // TODO: Valid email checking here
-        if (this.state.firstName == '' || this.state.lastName == '' || this.state.email == '') {
-            // TODO: Show a error message
+        //validate user input
+        if (!this.validateInput(this.state.email, this.state.firstName, this.state.lastName, this.state.phoneNumber)) {
             return;
         }
 
@@ -113,19 +161,23 @@ export default class Info extends React.Component {
                     <label>
                         First Name
                     <input type="text" name="firstName" onChange={this.handleChange.bind(this)} />
+                    <div>{this.state.errors.firstName}</div>
                     </label>
                     <label>
                         Last Name
                     <input type="text" name="lastName" onChange={this.handleChange.bind(this)} />
+                    <div>{this.state.errors.lastName}</div>
                     </label>
                     <br />
                     <label>
                         Email
                     <input type="email" name="email" onChange={this.handleChange.bind(this)} />
+                    <div>{this.state.errors.email}</div>
                     </label>
                     <label>
                         Phone number
                     <input type="tel" name="phoneNumber" onChange={this.handleChange.bind(this)} />
+                    <div>{this.state.errors.phoneNumber}</div>
                     </label>
                 </form>
 
