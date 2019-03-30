f0ff8c2892a690ec04fdd1230cac11ac1533b9d2
Jon Bockhorst <jmbockhorst@gmail.com>
2019-03-30 14:07:31 -0500

    Default is no email consent

--- src/main/resources/assets/src/infoPage.js
+++ src/main/resources/assets/src/infoPage.js
@@ -10,7 +10,7 @@ export default class Info extends React.Component {
             lastName: '',
             phoneNumber: '',
             email: '',
-            emailConsent: true,
+            emailConsent: false,
             errors: {}
         }
     }
