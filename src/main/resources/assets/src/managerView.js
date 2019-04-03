3807aca1df657e007f7d6d6d6deacbeb4a4c0aaf
Jon Bockhorst <jmbockhorst@gmail.com>
2019-04-02 23:56:26 -0500

    Created main manager view structure

--- src/main/resources/assets/src/managerView.js
+++ src/main/resources/assets/src/managerView.js
@@ -6,12 +6,17 @@ import './css/base.css';
 import './css/manager.css';
 import loadingImage from './images/loading.gif';
 import AppointmentsView from './managerView/appointmentsView';
+import ManagersView from './managerView/managersView';
+import ManagerSchedulingView from './managerView/managerSchedulingView';
+import BranchSchedulingView from './managerView/branchSchedulingView';
+import BranchHoursView from './managerView/branchHoursView';
 
 class ManagerView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             loading: false,
+            selectedId: 0,
         }
     }
 
@@ -21,19 +26,76 @@ class ManagerView extends React.Component {
         });
     }
 
+    changeMenu(i) {
+        this.setState({
+            selectedId: i,
+        })
+    }
+
     render() {
+        const sideBarItems = ["Appointments", "Managers", "Manager Scheduling", "Branch Scheduling", "Branch Hours"];
         return (
             <div>
                 <div id="sideBar">
-                    <div className="sideBarItem">Appointments</div>
-                    <div className="sideBarItem">Managers</div>
-                    <div className="sideBarItem">Manager Scheduling</div>
-                    <div className="sideBarItem">Branch Scheduling</div>
-                    <div className="sideBarItem">Branch Hours</div>
+                    {
+                        sideBarItems.map((item, i) => {
+                            return (
+                                <div key={i} className={this.state.selectedId == i ? "sideBarItem itemSelected" : "sideBarItem"} onClick={this.changeMenu.bind(this, i)}>
+                                    {item}
+                                    {
+                                        this.state.selectedId == i &&
+                                        <div className="selectedArrow"></div>
+                                    }
+                                </div>
+                            );
+                        })
+                    }
                 </div>
 
-                <AppointmentsView
-                    setStateValue={this.setStateValue.bind(this)} />
+                <CSSTransition // Show the appointments view
+                    in={this.state.selectedId == 0}
+                    timeout={400}
+                    classNames="view"
+                    unmountOnExit>
+                    <AppointmentsView
+                        setStateValue={this.setStateValue.bind(this)} />
+                </CSSTransition>
+
+                <CSSTransition // Show the managers view
+                    in={this.state.selectedId == 1}
+                    timeout={400}
+                    classNames="view"
+                    unmountOnExit>
+                    <ManagersView
+                        setStateValue={this.setStateValue.bind(this)} />
+                </CSSTransition>
+
+                <CSSTransition // Show the manager scheduling view
+                    in={this.state.selectedId == 2}
+                    timeout={400}
+                    classNames="view"
+                    unmountOnExit>
+                    <ManagerSchedulingView
+                        setStateValue={this.setStateValue.bind(this)} />
+                </CSSTransition>
+
+                <CSSTransition // Show the branch scheduling view
+                    in={this.state.selectedId == 3}
+                    timeout={400}
+                    classNames="view"
+                    unmountOnExit>
+                    <BranchSchedulingView
+                        setStateValue={this.setStateValue.bind(this)} />
+                </CSSTransition>
+
+                <CSSTransition // Show the branch hours view
+                    in={this.state.selectedId == 4}
+                    timeout={400}
+                    classNames="view"
+                    unmountOnExit>
+                    <BranchHoursView
+                        setStateValue={this.setStateValue.bind(this)} />
+                </CSSTransition>
 
                 <CSSTransition // Loading image for when we are retrieving data
                     in={this.state.loading}
---
+++ src/main/resources/assets/src/managerView/branchSchedulingView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class BranchSchedulingView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="mainViewHolder">Branch Scheduling View</div>
+        );
+    }
+}
