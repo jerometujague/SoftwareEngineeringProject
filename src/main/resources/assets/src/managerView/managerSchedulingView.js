56f3d4c42cf27b31a768eec0891d5b374eb2cea1
Jon Bockhorst <jmbockhorst@gmail.com>
2019-04-02 23:56:26 -0500

    Created main manager view structure

---
+++ src/main/resources/assets/src/managerView/managerSchedulingView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ManagerSchedulingView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="mainViewHolder">Manager Scheduling View</div>
+        );
+    }
+}
