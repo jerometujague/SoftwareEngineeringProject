cabe745c6329df8609cd6e2cea11972b1dbceee6
Jon Bockhorst <jmbockhorst@gmail.com>
2019-04-04 15:55:47 -0500

    Added ability to edit and cancel appointment

--- src/main/resources/assets/src/managerView/editDialog.js
+++ src/main/resources/assets/src/managerView/editDialog.js
@@ -3,17 +3,34 @@ import React from 'react'
 export default class EditDialog extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            editValues: this.props.editItems,
+        }
+    }
+
+    handleChange(i, e) {
+        const newEditValues = this.state.editValues;
+        newEditValues[i] = e.target.value;
+
+        this.setState({
+            editValues: newEditValues,
+        })
     }
 
     render() {
+        const styles = {
+            top: this.props.topPosition
+        }
+
         return (
-            <div className="editDialog">
+            <div className="editDialog" style={styles} key={this.props.editId}>
                 {
                     this.props.editItems.map((item, index) => {
-                        return <input key={index} type="text" defaultValue={item} />
+                        return <input key={index} type="text" defaultValue={item} className={this.props.editErrors[index] ? "editError" : ""} onChange={this.handleChange.bind(this, index)} />
                     })
                 }
-                <input type="submit" value="Save" onClick={this.props.saveHandler} />
+                <input type="submit" value="Save" onClick={this.props.saveHandler.bind(this, this.props.editId, this.state.editValues)} />
+                <input type="submit" value="X" onClick={this.props.closeHandler.bind(this)} />
             </div>
         );
     }
