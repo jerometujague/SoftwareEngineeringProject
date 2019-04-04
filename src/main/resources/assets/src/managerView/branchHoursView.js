4eeb5802dd3f61693bfa45dda5cbccca4475a936
jerometujague <jlt35400@ucmo.edu>
2019-04-04 11:30:16 -0500

    Basic Branch Hours View

--- src/main/resources/assets/src/managerView/branchHoursView.js
+++ src/main/resources/assets/src/managerView/branchHoursView.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default class BranchHoursView extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            branchHours: [],
+        }
+
+        this.loadHours();
+    }
+
+    async loadHours(){
+        this.props.setStateValue('loading', true);
+
+        let url = "/api/branchHours";
+
+        await $.getJSON(url, (branchHoursList) => {
+            const newBranchHours = [];
+            branchHoursList.forEach(branch => {
+                newBranchHours.push(branch);
+            });
+
+            this.setState({
+                branchHours: newBranchHours,
+            });
+        });
+        this.props.setStateValue('loading', false);
     }
 
     render() {
         return (
-            <div className="mainViewHolder">Branch Hours View</div>
+            <div className="mainViewHolder">
+                <h3>Branch Hours</h3>
+                {
+                    this.state.branchHours.map(branchHour => {
+                        return (
+                            <div key={branchHour.id} className="branchHours">
+                                <span className="branchHoursData">{branchHour.id}</span>
+                                <span className="branchHoursData">{branchHour.openTime}</span>
+                                <span className="branchHoursData">{branchHour.closeTime}</span>
+                                <span className="branchHoursData">{branchHour.branchId}</span>
+                                <span className="branchHoursData">{branchHour.dayOfWeek}</span>
+                            </div>
+                        );
+                    })
+                }
+
+            </div>
         );
     }
 }
