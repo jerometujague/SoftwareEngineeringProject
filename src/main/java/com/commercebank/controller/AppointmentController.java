5faa7f88571829ac231b7d4b0ab3651b5fdc287c
dbruce <dbruce@ucmo.edu>
2019-04-09 15:07:05 -0500

    moved email sending to MailContent class

--- src/main/java/com/commercebank/controller/AppointmentController.java
+++ src/main/java/com/commercebank/controller/AppointmentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.Arrays;
