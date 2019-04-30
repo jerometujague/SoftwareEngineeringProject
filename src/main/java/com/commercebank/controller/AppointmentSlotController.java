4065ca7c5a540c514c5e69909ca869489745856a
Jon Bockhorst <jmbockhorst@gmail.com>
2019-04-30 14:45:04 -0500

    Fixed phone number input and block hollidays

--- src/main/java/com/commercebank/controller/AppointmentSlotController.java
+++ src/main/java/com/commercebank/controller/AppointmentSlotController.java
@@ -143,8 +143,8 @@ public class AppointmentSlotController {
                         && h.getBranchId() == branchId)
                 .findFirst();
 
-        // If the branch has hours for the day
-        if(hours.isPresent()) {
+        // If the branch has hours for the day and its not a holliday
+        if(hours.isPresent() && !calendar.get(calendarId - 1).isHoliday()) {
             // Check if the current time is past the close time for current day
             if((currentTime.isAfter(hours.get().getCloseTime()) && calendarId == todayCalendarId) || (calendarId < todayCalendarId)){
                 return appointmentSlots;
